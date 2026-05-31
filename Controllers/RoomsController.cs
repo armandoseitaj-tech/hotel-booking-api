e144d18ce320476ace43578be0e323cf39f2bf6e
armandoseitaj-tech <armando.seitaj@fshnstudent.info>
2026-05-31 17:00:48 +0200

    Update RoomsController.cs

--- Controllers/RoomsController.cs
+++ Controllers/RoomsController.cs
@@ -1,1 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
 
+namespace HotelBookingApi.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class RoomsController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult GetAll()
+    {
+        return Ok("All Rooms");
+    }
+}
