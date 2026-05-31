c4cdf1d1ed77af373327c8f6818687b13916db49
armandoseitaj-tech <armando.seitaj@fshnstudent.info>
2026-05-31 16:36:03 +0200

    Add Reservation model for hotel bookings

---
+++ Models/Reservation.cs
@@ -0,0 +1,14 @@
+namespace HotelBookingApi.Models;
+
+public class Reservation
+{
+    public int Id { get; set; }
+
+    public int CustomerId { get; set; }
+
+    public int RoomId { get; set; }
+
+    public DateTime CheckInDate { get; set; }
+
+    public DateTime CheckOutDate { get; set; }
+}
