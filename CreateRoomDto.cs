e868110ac88696f520485cfc869cec725c3a14dd
armandoseitaj-tech <armando.seitaj@fshnstudent.info>
2026-05-31 16:49:02 +0200

    Create CreateRoomDto.cs

---
+++ CreateRoomDto.cs
@@ -0,0 +1,12 @@
+namespace HotelBookingApi.DTOs;
+
+public class CreateRoomDto
+{
+    public string RoomNumber { get; set; } = string.Empty;
+
+    public decimal PricePerNight { get; set; }
+
+    public bool IsAvailable { get; set; }
+
+    public int HotelId { get; set; }
+}
