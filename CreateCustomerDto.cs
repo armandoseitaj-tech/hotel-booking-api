51af5e01d6ee8cf41a5ce3ee4ff141665b2e2e0d
armandoseitaj-tech <armando.seitaj@fshnstudent.info>
2026-05-31 16:50:54 +0200

    Add CreateCustomerDto class for customer data

---
+++ CreateCustomerDto.cs
@@ -0,0 +1,10 @@
+namespace HotelBookingApi.DTOs;
+
+public class CreateCustomerDto
+{
+    public string FullName { get; set; } = string.Empty;
+
+    public string Email { get; set; } = string.Empty;
+
+    public string PhoneNumber { get; set; } = string.Empty;
+}
