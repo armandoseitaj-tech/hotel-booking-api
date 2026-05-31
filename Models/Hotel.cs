0aec2bb25b8e7a402408af82c2a5d707e8cf7354
armandoseitaj-tech <armando.seitaj@fshnstudent.info>
2026-05-31 17:17:20 +0200

    Create Hotel.cs

---
+++ Models/Hotel.cs
@@ -0,0 +1,9 @@
+namespace HotelBookingApi.Models;
+
+public class Hotel
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = "";
+    public string City { get; set; } = "";
+    public string Address { get; set; } = "";
+}
